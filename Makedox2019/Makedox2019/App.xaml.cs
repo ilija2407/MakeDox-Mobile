3f16791ef26b8806d59fe3b307b39dcaab954844
ilija <ilijarushkovski@gmail.com>
2019-07-10 09:34:10 +0200

    Setting up project

---
+++ Makedox2019/Makedox2019/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using FreshMvvm;
+using Makedox2019.PageModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Makedox2019
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var page = FreshPageModelResolver.ResolvePageModel<UpcomingEventsPageModel>();
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Makedox2019/Makedox2019/PageModels/UpcomingEventsPageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Makedox2019.PageModels
+{
+    public class UpcomingEventsPageModel : FreshMvvm.FreshBasePageModel
+    {
+        public UpcomingEventsPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+    }
+}
