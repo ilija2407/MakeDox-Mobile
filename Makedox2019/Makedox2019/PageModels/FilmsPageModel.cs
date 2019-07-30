64e0524a5d97fe1e31d4a4e14cd68fc2ab290004
ilija <ilijarushkovski@gmail.com>
2019-07-30 13:51:24 +0200

    search bar

--- Makedox2019/Makedox2019/PageModels/FilmsPageModel.cs
+++ Makedox2019/Makedox2019/PageModels/FilmsPageModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Makedox2019.Models;
+using Realms;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -8,12 +11,90 @@ namespace Makedox2019.PageModels
 {
     public class FilmsPageModel : FreshMvvm.FreshBasePageModel
     {
+        #region Properties
+        public List<Movie> MoviesList { get; set; }
+
+        private List<Movie> filteredList;
+        public List<Movie> FilteredList
+        {
+            get { return filteredList; }
+            set
+            {
+                filteredList = value;
+                RaisePropertyChanged(nameof(FilteredList));
+            }
+        }
+
+        private bool showMoviesList;
+        public bool ShowMoviesList
+        {
+            get
+            {
+                return showMoviesList;
+            }
+            set
+            {
+                showMoviesList = value;
+                RaisePropertyChanged(nameof(ShowMoviesList));
+            }
+        }
+
+        private string searchText;
+        public string SearchText
+        {
+            get
+            {
+                return searchText;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (MoviesList != null)
+                    {
+                        searchText = value;
+                        if (value != "")
+                        {
+                            ShowMoviesList = true;
+
+                        }
+                        else
+                        {
+                            ShowMoviesList = false;
+
+                        }
+
+                        var tempRecords = MoviesList.Where(i => i.Title.ToLower().Contains(searchText.ToLower()) || i.Title.ToLower().Contains(searchText.ToLower())).OrderBy(i => i.StartTime);
+
+                        FilteredList = new List<Movie>(tempRecords);
+                    }
+                    {
+                        searchText = value;
+                    }
+
+                }
+                else
+                {
+                    ShowMoviesList = false;
+                    searchText = null;
+
+                }
+
+                RaisePropertyChanged(nameof(SearchText));
+
+            }
+        }
+        #endregion
+
         #region Commands
         public ICommand NavigateToUpcomingEventsPageCommand { get; set; }
         public ICommand NavigateToTimeLinePageCommand { get; set; }
         public ICommand NavigateToMakedoxPageCommand { get; set; }
         public ICommand NavigateToMenuPageCommand { get; set; }
         public ICommand NavigateToFilmsPageCommand { get; set; }
+        public ICommand SearchCommand { get; private set; }
+        public ICommand MovieSelectedCommand { get; private set; }
+
         #endregion
 
         public FilmsPageModel()
@@ -24,6 +105,15 @@ namespace Makedox2019.PageModels
         public override void Init(object initData)
         {
             base.Init(initData);
+
+            
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+            var db = Realm.GetInstance();
+            MoviesList = db.All<Movie>().OrderBy(x => x.StartTime).ToList();
         }
 
 
@@ -36,6 +126,25 @@ namespace Makedox2019.PageModels
             NavigateToTimeLinePageCommand = new Command(NavigateToTimeLinePage);
             NavigateToMakedoxPageCommand = new Command(NavigateToMakedoxPage);
             NavigateToMenuPageCommand = new Command(NavigateToMenuPage);
+            SearchCommand = new Command<object>(Search);
+            MovieSelectedCommand = new Command<object>(OnMovieSelected);
+
+        }
+
+        private void Search(object obj)
+        {
+            //CoreMethods.PushPageModel<SearchResultsPageModel>(FilteredList);
+        }
+
+
+        private void OnMovieSelected(object obj)
+        {
+            var item = obj as Movie;
+            if (item != null)
+            {
+                ShowMoviesList = false;
+                //CoreMethods.PushPageModel<ProductPageModel>(item);
+            }
 
         }
 
