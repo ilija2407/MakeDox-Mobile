56009a74e8d84a29f0b869f4843085f76960bd3d
Ice Jovanoski <ijovanoski@outlook.com>
2019-08-13 08:25:02 +0200

    Fix iOS Searchbar

---
+++ Makedox2019/Makedox2019.iOS/Renderers/CustomSerachBarRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using Makedox2019.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(CustomSerachBarRenderer))]
+namespace Makedox2019.iOS.Renderers
+{
+    public class CustomSerachBarRenderer : SearchBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+            UITextField.AppearanceWhenContainedIn(typeof(UISearchBar)).BackgroundColor = UIColor.Clear;
+        }
+    }
+}
