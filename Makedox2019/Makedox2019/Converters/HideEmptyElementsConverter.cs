d97b7a8582331006bdd8729446b525e4b0181d23
Ice Jovanoski <ijovanoski@outlook.com>
2019-08-14 17:43:35 +0200

    Hide empty details in EventDetailsPAge

---
+++ Makedox2019/Makedox2019/Converters/HideEmptyElementsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Makedox2019.Converters
+{
+    public class HideEmptyElementsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            return !string.IsNullOrEmpty(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
