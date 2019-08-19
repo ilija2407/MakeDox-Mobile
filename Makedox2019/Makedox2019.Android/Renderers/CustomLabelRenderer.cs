561fc7787e172c093a89187b96ee809b32924b67
Ice Jovanoski <ijovanoski@outlook.com>
2019-08-19 21:39:52 +0200

    Add android custom label renderer & milenium venues resource

---
+++ Makedox2019/Makedox2019.Android/Renderers/CustomLabelRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Makedox2019.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace Makedox2019.Droid.Renderers
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if(e.NewElement != null)
+            {
+                Control.Typeface = e.NewElement.FontAttributes == FontAttributes.Bold ? Typeface.CreateFromAsset(this.Context.Assets, "Intro-Bold.otf") : Typeface.CreateFromAsset(this.Context.Assets, "Intro-Regular.otf");
+                Control.SetTextSize(Android.Util.ComplexUnitType.Sp, (float)e.NewElement.FontSize);
+            }
+        }
+    }
+}
