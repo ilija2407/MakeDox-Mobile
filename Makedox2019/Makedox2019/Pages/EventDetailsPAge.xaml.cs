8d7124948ce46d151c597c29c94942f259519b77
Ilija Rushkovski <ilija@servsuite.net>
2019-08-19 09:38:01 +0200

    event page fix for movie frame

--- Makedox2019/Makedox2019/Pages/EventDetailsPAge.xaml.cs
+++ Makedox2019/Makedox2019/Pages/EventDetailsPAge.xaml.cs
@@ -17,6 +17,21 @@ namespace Makedox2019.Pages
         {
             On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
             InitializeComponent();
+            var height = App.ScreenHeight;
+            if (height > 700)
+            {
+                productImg.HeightRequest = 400;
+                productImgBorder.HeightRequest = 402;
+
+                // vendorProductList.Margin = new Thickness(20, 5, 20, 70);
+                // myProductsList.Margin = new Thickness(0, 60, 0, 0);  
+            }
+            else
+            {
+                productImg.HeightRequest = 300;
+                productImgBorder.HeightRequest = 302;
+                //vendorProductList.Margin = new Thickness(20, 5, 20, 70);
+            }
         }
     }
 }
